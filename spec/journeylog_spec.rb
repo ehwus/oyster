1f99dd97d40b10433a8bf4613a583049e941479a
Alex Withington-Smith <alex@awithsmith.com>
2020-11-06 08:55:33 +0000

    added a test for finishing the journey

--- spec/journeylog_spec.rb
+++ spec/journeylog_spec.rb
@@ -2,7 +2,7 @@ require 'journeylog'
 
 describe JourneyLog do
   let(:station) { double :station }
-  let(:journey) { double :journey, start: station, finish: station }
+  let(:journey) { double :journey }
   let(:journey_class) { double :journey_class, new: journey }
 
   describe "#start" do
@@ -10,6 +10,16 @@ describe JourneyLog do
       expect(journey_class).to receive(:new)
       expect(journey).to receive(:start=).with(station)
       JourneyLog.new(journey_class).start(station)
+    end
+  end
+
+  describe "#finish" do
+    it "finishes a journey" do
+      test = JourneyLog.new(journey_class)
+      allow(journey).to receive(:start=).with(station)
+      test.start(station)
+      expect(journey).to receive(:finish=).with(station)
+      test.finish(station)
     end
   end
 end
